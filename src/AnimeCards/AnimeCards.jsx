3b3f3fddd9f273e52a5822777231fa195b9df6c0
Malik Soltukiev <freelance99@bk.ru>
2020-11-21 15:57:05 +0300

    небольшой рефакторинг фильрации царт

--- src/AnimeCards/AnimeCards.jsx
+++ src/AnimeCards/AnimeCards.jsx
@@ -1,19 +1,20 @@
-import React from "react"
-import { AnimeCard } from "./AnimeCard/AnimeCard"
-import s from "./AnimeCards.module.scss"
-import { useSelector } from "react-redux"
-import { dataCards } from "./dataCards"
+import React from 'react'
+import { AnimeCard } from './AnimeCard/AnimeCard'
+import s from './AnimeCards.module.scss'
+import { useSelector } from 'react-redux'
+import { dataCards } from './dataCards'
 
 export const AnimeCards = () => {
-  const categories = useSelector(state =>
-    state.categories.filter(category => category.selected).map(category => category.title.toLocaleLowerCase())
-  )
-  const filterCards = dataCards.filter(card => categories.every(category => card.category.some(e => e === category)))
+  const categories = useSelector(state => state.categories)
+  const selectedCategoriesNames = categories
+    .filter(category => category.selected)
+    .map(category => category.title.toLocaleLowerCase())
+  const filteredCards = dataCards.filter(card => selectedCategoriesNames.every(selected => card.category.some(e => e === selected)))
 
   return (
     <section className={s.animeCards}>
       <div className={s.cards}>
-        {filterCards.map(({ id, category, name, avatar }) => (
+        {filteredCards.map(({ id, category, name, avatar }) => (
           <AnimeCard key={id} category={category} name={name} avatar={avatar} />
         ))}
       </div>
